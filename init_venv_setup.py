b8cc5dc5579a3f1b5e18c6fae6174fba97e6752f
Kaan Uctum <kaan.uctum@users.noreply.github.com>
2021-12-17 11:54:06 +0100

    Update init_venv_setup.py

--- init_venv_setup.py
+++ init_venv_setup.py
@@ -1,15 +1,15 @@
 from subprocess import run, PIPE, STDOUT
 
-cmd = "pip install -r requirements.txt"
-ps = run(cmd, stdout=PIPE, stderr=STDOUT, shell=True, text=True)
+ps = run("pip install -r requirements.txt",
+         stdout=PIPE, stderr=STDOUT, shell=True, text=True)
 print(ps.stdout)
 
-cmd = "python -m spacy download en_core_web_lg"
-ps = run(cmd, stdout=PIPE, stderr=STDOUT, shell=True, text=True)
+ps = run("python -m spacy download en_core_web_lg",
+         stdout=PIPE, stderr=STDOUT, shell=True, text=True)
 print(ps.stdout)
 
 
-cmd = "python -m spacy download en_core_web_sm"
-ps = run(cmd, stdout=PIPE, stderr=STDOUT, shell=True, text=True)
+ps = run("python -m spacy download en_core_web_sm",
+         stdout=PIPE, stderr=STDOUT, shell=True, text=True)
 print(ps.stdout)
 
