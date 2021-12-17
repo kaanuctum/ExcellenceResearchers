9867d401f55a302511916ea8a64fbf8c25733a76
Kaan Uctum <kaan@uctum.com>
2021-12-17 06:28:30 +0100

    Delete TrainModels2.py

--- MainScripts/TrainModels2.py
+++
@@ -1,7 +0,0 @@
-from HelperObjects.ModelTrainer import ModelTrainer
-from multiprocessing import freeze_support
-
-if __name__ == '__main__':
-    freeze_support()
-    mt = ModelTrainer(force_re_parse=False, worker_count = 8)
-    mt.find_optimal_values()
