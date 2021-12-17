26b63178148364a8f340ac8b93f36cf4d9fd7f43
Kaan Uctum <kaan@uctum.com>
2021-12-17 05:34:13 +0100

    Update TrainModels2.py

--- MainScripts/TrainModels2.py
+++ MainScripts/TrainModels2.py
@@ -1,32 +1,7 @@
-
-
-
-def train_model_auto(args):
-    num_topics, ns = args
-    print(f'training with {num_topics} number of topics')
-
-    chunk_size = (2 ** 14)
-    passes = 30
-    iterations = 600
-
-    lda_model = gensim.models.LdaModel(corpus=ns.CORPUS,
-                                       id2word=ns.ID2WORD,
-                                       chunksize=chunk_size,
-                                       alpha='auto',
-                                       eta='auto',
-                                       iterations=iterations,
-                                       num_topics=num_topics,
-                                       passes=passes,
-                                       eval_every=None
-                                       )
-    print(f'finished training with {num_topics} number of topics ')
-    return lda_model
-
-
-
-
+from HelperObjects.ModelTrainer import ModelTrainer
+from multiprocessing import freeze_support
 
 if __name__ == '__main__':
     freeze_support()
-    mt = ModelTrainer(force_re_parse=False)
+    mt = ModelTrainer(force_re_parse=False, worker_count = 8)
     mt.find_optimal_values()
