e17123109bf3883189ba5556c1abf6c558c4f9c1
Kaan Uctum <kaan@uctum.com>
2021-12-18 04:31:45 +0100

    Update AnalyzeData.py

--- MainScripts/AnalyzeData.py
+++ MainScripts/AnalyzeData.py
@@ -1,8 +1,15 @@
+import pingouin as pt
 from HelperObjects.Analyzer import Analyzer
 analyzer = Analyzer()
 data = analyzer.get_prepared_data()
 
-zero_after_count = data.loc[data["size_after"] == 0].shape[0]
-zero_before_count = data.loc[data["size_before"] == 0].shape[0]
+zero_after_count = data.loc[data["size_after"] <= 1].shape[0]
+zero_before_count = data.loc[data["size_before"] <= 1].shape[0]
 
 filtered = data.loc[(data["size_after"] > 1) & (data["size_before"] > 1)]
+
+
+print(filtered.describe())
+
+res = pt.ttest(filtered["avg_dist_before"], filtered["avg_dist_after"], paired=True)
+print(res)
