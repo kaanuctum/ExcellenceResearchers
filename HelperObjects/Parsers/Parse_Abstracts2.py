c5c3694259dcd2330f88e97b7146f84d9573e2d1
Kaan Uctum <kaan@uctum.com>
2022-01-27 01:41:52 +0100

    update the copyright removal algorithm

--- HelperObjects/Parsers/Parse_Abstracts2.py
+++ HelperObjects/Parsers/Parse_Abstracts2.py
@@ -1,18 +1,15 @@
 from HelperObjects import *
-import spacy
 import nltk
 import gensim
-import pandas as pd
 import re
 from tqdm import tqdm
-import pickle
-import time
+
 """
  These are the helper methods used 
 """
 sql.connect()
 STOPWORDS = nltk.corpus.stopwords.words('english')
-STOPWORDS.extend(['however', 'compare', 'differ', 'find', 'year'])
+STOPWORDS.extend(['however', 'compare', 'differ', 'find', 'year', 'use'])
 
 
 # just gives the eid and the raw abstracts of all articles
@@ -28,24 +25,35 @@ def clean_copyright(raw_abstracts):
 
 
 def clean_copyright_just_text(raw_abstract, copyright_threshold=50):
-    # if the copyright data is not present in the text, there isn't much we can do to find the copyright
+    # if the copyright symbol is not present in the text, there isn't much we can do to find the copyright
     raw_abstract = raw_abstract.strip()
     if raw_abstract.find('©') == -1: return raw_abstract
 
-    phrases_to_delete = ['All rights reserved.', 'GmbH & Co. KGaA', 'All Rights Reserved']
+    phrases_to_delete = ['All rights reserved', 'GmbH & Co. KGaA', 'All Rights Reserved']
 
     cleaned = ''
     abstract = raw_abstract.replace('B.V', 'BV')
     for phrase in phrases_to_delete:
         abstract = abstract.replace(phrase, '')
-
+    abstract = abstract.replace('The Author(s)', 'The Authors')
     # some copyright sentences are not separated properly
 
-    # replace all the dots are followed by a capitol letter
+    # add a space to all the dots are followed by a capitol letter
     abstract = re.sub(r'[.](?=[A-Z])(?=[^.])', '. ', abstract).strip()
 
-    # find all the upper case letters that follow a small case letter without a space
-    abstract = re.sub(r'([a-z](?=[A-Z])(?=[^\s-]))', r'\1. ', abstract)
+    # find all the upper case letters or numbers that follow a small case letter without a space
+    # abstract = re.sub(r'([a-z](?=[A-Z])(?=[^\s-]))', r'\1. ', abstract)
+
+    # find all the beginnings where the first char is the copyright symbol, followed by some space if need be (usually 1)
+    # then having a word that has no uppercase in it(either all small or small plus numbers) then having a capital letter
+    # next to it that is not seperated by a space, and itself is not followed by a space\
+    # e.g. working:
+    # © 2019This paper
+    # © asdThis paper
+    # not working:
+    # © 2019 This paper
+    # © 2019.This paper
+    abstract = re.sub(r'([a-z0-9](?=[A-Z])(?=[^\s-]))', r'\1. ', abstract)
 
     # if there is a separation where only integers follow after an copyright sign, this is the date, the rest is also
     # a part of the copy right info, can be removed
@@ -53,32 +61,19 @@ def clean_copyright_just_text(raw_abstract, copyright_threshold=50):
     if abstract.find('©') > 0:
         abstract = abstract.replace('©', '. ©')
 
+    # because there are no consistency between the different publications and their copyrights, there will be some cases
+    # where the first line is dropped, so that in any other cases the copyright info does not creep into the
+    # lemmatized version. This is a sacrifice I am willing to make
+
+
     # iterate over the sentences in the abstract
     for i in abstract.split('.'):# nltk.sent_tokenize(abstract):
         i = i.strip()
-        if i.find('©') == 0 and len(i) < copyright_threshold:
-            # if the symbol is the first char and the sentence is less then 50 chars
-            # assume all the sentence is just copyright info
+        if i.find('©') == 0:
+            # assume all of the sentence is just copyright info
             continue
         else:
-            cleaned += i
-
-
-
-
-    # this is a less elegant solution, but it at this point I would rather cut too much from the text
-    # rather than leave too much copyright in it
-    # after a point the amount of If-cases I have to open would be the same as if I just cleaned them by hand
-    dirty = cleaned.find("©") >= 0
-    if dirty:
-        whole = ''
-        parts = cleaned.split('.')
-        for part in parts:
-            if part.strip().find("©") != 0:
-                whole += part.strip() + ' '
-            else:
-                continue  # print(part)
-        cleaned = whole
+            cleaned += i + ' '
     return cleaned
 
 
@@ -117,12 +112,15 @@ def sent_to_words(sentences):
     return output
 
 
-def words_to_sent(word_lists):
+def bow_to_sentence(word_lists, id2word):
     output = list()
     for word_list in word_lists:
         sen = ''
         for word in word_list:
-            sen += (' ' + word)
+            try:
+                sen += (' ' + id2word[word[0]])
+            except KeyError:
+                continue
         output.append(sen.strip())
     return output
 
